8ff26d85619c156287ec5a6fde60ad2dd97dd11b
unknown <Administrator@20091108-1157.(none)>
2011-12-27 01:48:22 +0800

    first commit

---
+++ app/controllers/BaseController.java
@@ -0,0 +1,21 @@
+package controllers;
+
+import play.mvc.Before;
+import play.mvc.Controller;
+import play.mvc.Http;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-12-26
+ * Time: 下午10:34
+ * To change this template use File | Settings | File Templates.
+ */
+public class BaseController extends Controller {
+    protected static ThreadLocal<String> template = new ThreadLocal<String>();
+    @Before
+    static void getTemplatePath() {
+        System.out.println(Http.Request.current().headers.get("user-agent").value());
+        template.set(Http.Request.current().controller+"/iphone/"+Http.Request.current().actionMethod+".html");
+    }
+}
