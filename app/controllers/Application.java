2b08a4db14755b7a6c44d06f7808dc3c78c1744c
sufeng <sufengster@gmail.com>
2011-12-29 02:31:36 +0800

    2011-12-29 start on jquery mobile2011-12-29 start on jquery mobile

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,6 +2,9 @@ package controllers;
 
 //import models.*;
 
+import models.User;
+import play.data.validation.Valid;
+
 public class Application extends BaseController {
     //test
     public static void index() {
@@ -9,7 +12,19 @@ public class Application extends BaseController {
     }
 
     public static void register() {
-        render();
+        render(template.get());
+    }
+
+    public static void saveUser(@Valid User user, String verifyPassword) {
+        validation.required(verifyPassword);
+        validation.equals(verifyPassword, user.password).message("Your password doesn't match");
+        if(validation.hasErrors()) {
+            render(getTemplatePath("register"), user, verifyPassword);
+        }
+        user.create();
+        session.put("user", user.nickname);
+        flash.success("Welcome, " + user.nickname);
+        index();
     }
 
 }
--- app/controllers/BaseController.java
+++ app/controllers/BaseController.java
@@ -15,8 +15,16 @@ public class BaseController extends Controller {
     protected static ThreadLocal<String> template = new ThreadLocal<String>();
     @Before
     static void getTemplatePath() {
-        System.out.println(Http.Request.current().headers.get("User-Agent").value());
+//        System.out.println(Http.Request.current().headers.get("user-agent").value());
         // test branch.sufeng
-        template.set(Http.Request.current().controller+"/iphone/"+Http.Request.current().actionMethod+".html");  //get path
+        template.set(Http.Request.current().controller+"/"+getVersion()+"/"+Http.Request.current().actionMethod+".html");  //get path
+    }
+    
+    static String getTemplatePath(String template){
+        return "@"+Http.Request.current().controller+"."+getVersion()+"."+template;
+    }
+
+    static String getVersion(){
+        return "iphone";
     }
 }
