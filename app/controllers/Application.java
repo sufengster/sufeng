366a36c73158d83d7888c0188a8c0fc604cb4d7e
sufeng <sufengster@gmail.com>
2011-12-28 14:00:09 +0800

    optimize the imports

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,11 +1,6 @@
 package controllers;
 
-import play.*;
-import play.mvc.*;
-
-import java.util.*;
-
-import models.*;
+//import models.*;
 
 public class Application extends BaseController {
     //test
