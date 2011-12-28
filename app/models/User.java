5c191e1fcdf59fa210d0aa44afe6232aa27e6357
sufeng <sufengster@gmail.com>
2011-12-28 17:47:28 +0800

    add some models

---
+++ app/models/User.java
@@ -0,0 +1,11 @@
+package models;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-12-28
+ * Time: 下午2:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class User {
+}
