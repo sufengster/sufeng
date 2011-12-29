ea1b6fb60548ab70da1e14c69d33d298a1d40c59
sufeng <sufengster@gmail.com>
2011-12-29 23:15:19 +0800

    modify login,logout page

---
+++ app/config/Constant.java
@@ -0,0 +1,11 @@
+package config;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Administrator
+ * Date: 11-12-29
+ * Time: 下午9:16
+ * To change this template use File | Settings | File Templates.
+ */
+public class Constant {
+}
